450352dc6780d3581fabdeb7ebbdafefdca7d8de
kevin.romero <kevin.romero@onbusinessconsulting.com>
2020-05-16 02:33:16 -0500

    add CustomerGrid

--- src/components/CustomersList.js
+++ src/components/CustomersList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import CustomersListItem from './CustomerListItem';
+//import CustomersListItem from './CustomerListItem';
+import CustomerGrid from './CustomerGrid';
 import { CUSTOMER_LIST } from '../constants/permissions';
 import { accessControl } from './../helpers/accessControl';
 
@@ -8,15 +9,7 @@ const CustomersList = ({ customers, urlPath }) => {
     return (
         <div className="customers-list">
             {
-                customers.map(c =>
-                    <CustomersListItem
-                        key={c.dni}
-                        dni={c.dni}
-                        name={c.name}
-                        editAction={'Editar'}
-                        delAction={'Eliiminar'}
-                        urlPath={urlPath}>
-                    </CustomersListItem>)
+                <CustomerGrid rows={customers} urlPath={urlPath}/>                
             }
         </div>
     );
